ec0c116b4e75f027e43f1745479944e47df48256
Acc-one <55378268+Acc-one@users.noreply.github.com>
2019-09-25 21:24:30 +0300

    Delete 1.2 .py

--- 1.2 .py
+++
@@ -1,9 +0,0 @@
-a = float (input ('a = '))
-b = float (input ('b = '))
-c = float (input ('c = '))
-d = float (input ('d = '))
-f = float (input ('f = '))
-if f - d:
-    print ('Делить на ноль нельзя')
-else:
-    print ((a*b - c) / (f - g))
