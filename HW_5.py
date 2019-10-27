2d13fa6d574d5d07be2a1ed9dc531c4f93445d95
Acc-one <55378268+Acc-one@users.noreply.github.com>
2019-10-28 01:17:51 +0300

    Add files via upload

---
+++ HW_5.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+def doter(a):
+    return not len(a) % 2 and a[0] == 'Д'
+
+
+def bfs(dq, people):
+    visit = []
+    while dq:
+        vekt = dq.popleft()
+        if vekt not in visit:
+            if doter(vekt):
+                return vekt
+            else:
+                dq += people.get(vekt, [])
+            visit.append(vekt)
+    return 'Никто не найден'
+
+
+fr = {
+    'Женя': ['Рустам', 'Виталий', 'Алексей',],
+    'Рустам': ['Дима', 'Саша'],
+    'Виталий': ['Женя'],
+    'Саша': ['Рустам', 'Дима']
+}
+
+d = deque(fr['Женя'])
+
+print(bfs(d, fr))
