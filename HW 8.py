f92ad39eda5b56be3bd30e790b4da25c93910d35
Acc-one <55378268+Acc-one@users.noreply.github.com>
2020-02-15 13:43:20 +0300

    Update HW 8.py

--- HW 8.py
+++ HW 8.py
@@ -4,14 +4,14 @@ class Date:
         self.month = None
         self.day = None
 
-     def date_ent(self):
+    def date_ent(self):
         self.year = input('Year: ')
         self.month = input('Month: ')
         self.day = input('Day: ')
 
-     def __str__(self):
+    def __str__(self):
         return self.year, self.month, self.day, sep='.' 
 
- d = Date()
+d = Date()
 d.date_ent()
-d.__str__()
+d. ()
